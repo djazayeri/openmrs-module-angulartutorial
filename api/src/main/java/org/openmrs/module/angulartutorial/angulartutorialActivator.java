4a7d6792cea331c73d0b72ed06b7f9a41ff2554b
Darius Jazayeri <jazayeri@alum.mit.edu>
2015-12-08 10:37:09 +0800

    Examples 1-3

--- api/src/main/java/org/openmrs/module/angulartutorial/angulartutorialActivator.java
+++ api/src/main/java/org/openmrs/module/angulartutorial/angulartutorialActivator.java
@@ -9,10 +9,7 @@
  */
 package org.openmrs.module.angulartutorial;
 
-
-import org.apache.commons.logging.Log; 
-import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.BaseModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
